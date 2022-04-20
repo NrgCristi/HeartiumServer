534b8c8511470dbcf1c2fed68bc6bc984b0203ad
CristianCosticea <97525096+CristianCosticea@users.noreply.github.com>
2022-04-20 22:29:59 +0300

    Update version.js

--- structure/version.js
+++ structure/version.js
@@ -7,31 +7,31 @@ express.get("/fortnite/api/version", async (req, res) => {
         "serverDate": new Date().toISOString(),
         "overridePropertiesVersion": "unknown",
         "cln": "17951730",
-        "build": "444",
+        "build": "1.8",
         "moduleName": "Fortnite-Core",
         "buildDate": "2021-10-27T21:00:51.697Z",
         "version": "18.30",
-        "branch": "Release-18.30",
+        "branch": "Release-1.8",
         "modules": {
           "Epic-LightSwitch-AccessControlCore": {
             "cln": "17237679",
-            "build": "b2130",
+            "build": "1.8",
             "buildDate": "2021-08-19T18:56:08.144Z",
-            "version": "1.0.0",
+            "version": "1.8",
             "branch": "trunk"
           },
           "epic-xmpp-api-v1-base": {
             "cln": "5131a23c1470acbd9c94fae695ef7d899c1a41d6",
-            "build": "b3595",
+            "build": "1.8",
             "buildDate": "2019-07-30T09:11:06.587Z",
-            "version": "0.0.1",
+            "version": "1.8",
             "branch": "master"
           },
           "epic-common-core": {
             "cln": "17909521",
-            "build": "3217",
+            "build": "1.8",
             "buildDate": "2021-10-25T18:41:12.486Z",
-            "version": "3.0",
+            "version": "1.8",
             "branch": "TRUNK"
           }
         }
